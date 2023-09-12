2bc525ff24a4ac0447762b47c9ed123fff5636d4
Lun <dima.brysh@gmail.com>
2023-09-12 22:20:35 +0300

    latest changes...

--- Lab №2/№1.py
+++ Lab №2/№1.py
@@ -1,16 +1,30 @@
 from math import sqrt
 
 def natural_number():
+    print("Введите номер простого числа, который хотите увидеть: ")
     while True:
-        num = input("Введите номер простого числа, который хотите увидеть: ")
-        if num.isdecimal():
-            num = int(num)
-            if num > 0:
-                break
-            else:
-                print("Нужно ввести целое число больше 0, повторите ввод")
+        # num = input("Введите номер простого числа, который хотите увидеть: ")
+        # if num.isdecimal():
+        #     num = int(num)
+        #     if num > 0:
+        #         break
+        #     else:
+        #         print("Нужно ввести целое число больше 0, повторите ввод")
+        # else:
+        #     print("Некорректный ввод, пожалуйста повторите")
+        num = input()
+        try:
+            int(num)
+        except:
+            print("Вы ввели не целое число! Повторите ввод:")
         else:
-            print("Некорректный ввод, пожалуйста повторите")
+            num = int(num)
+            if num <= 0:
+                print("Число должно быть больше нуля! Повторите ввод:")
+            else:
+                break
+
+
     return num
 
 def prime(position):
